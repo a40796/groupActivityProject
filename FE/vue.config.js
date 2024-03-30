97ef389b0fce7af038be6ec30c87f10d05bfbbf7
albert huang <d0191512@gmail.com>
2024-03-30 14:26:30 +0800

    defineConfig

--- FE/vue.config.js
+++ FE/vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('vue-cli-plugin-eslint/node_modules/@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true
 })
