8314dc3fceab92577c01a65b0366185c79ed00b8
albert huang <d0191512@gmail.com>
2024-03-30 22:17:21 +0800

    bridge

--- BE/app.js
+++ BE/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: 'http://localhost:8080', 
+  origin: 'http://localhost:3000', 
   credentials:true
 }));
 
