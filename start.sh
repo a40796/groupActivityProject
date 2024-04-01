5c0709771028389e42b242676d806ec1544c6b70
albert <pinghunghuang@PingdeMacBook-Pro.local>
2024-04-01 21:49:03 +0800

    revert script

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# 啟動後端服務
-cd /home/app/BE
-npm start &
+cd ./BE
+npm install
+cd ..
 
-# 在另一個端口上提供前端應用
-cd /home/app/FE
-serve -s dist -l 8080 &
+cd ./FE
+npm install
+cd ..
 
-# 等待後台任務結束（這將不會發生，除非有一個服務終止）
-wait
+npm install
+npm run start-all
