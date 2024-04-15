0faf103c83895869c558de0f6550a7a8156b03f7
albert <pinghunghuang@PingdeMacBook-Pro.local>
2024-04-15 23:39:57 +0800

    static

--- BE/app.js
+++ BE/app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'FE', 'dist')));
 app.use(cors({
   origin: 'http://localhost:9090', 
   credentials:true
